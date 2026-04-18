ad2cab7c614f2f31f3abe2bd3681c1a8814dd788
HARE KRISHNA <snehasis>
2026-04-18 20:54:30 +0530

    firts

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
-let temperature = 0;
+let data = {
+    temperature: 0,
+    voltage: 0,
+    ax1: 0,
+    ay1: 0,
+    az1: 0,
+    ax2: 0,
+    ay2: 0,
+    az2: 0
+};
+
 let clients = [];
 
-let sneha = 0;
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/Index.html');
+    res.sendFile(path.join(__dirname, 'public', 'Index.html'));
 });
 
-app.get('/api/temp', (req, res) => {
-    res.json({ temperature });
+app.get('/api/data', (req, res) => {
+    res.json(data);
 });
 
 app.get('/api/stream', (req, res) => {
@@ -29,7 +38,7 @@ app.get('/api/stream', (req, res) => {
     const clientId = Date.now();
     clients.push({ id: clientId, res });
 
-    res.write(`data: ${JSON.stringify({ temperature })}\n\n`);
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
 
     req.on('close', () => {
         clients = clients.filter(c => c.id !== clientId);
@@ -37,15 +46,23 @@ app.get('/api/stream', (req, res) => {
 });
 
 app.get('/api/change', (req, res) => {
-    temperature = Number(req.query.temperature) || 0;
-    sendTemperatureUpdate();
-    res.json({ temperature });
+    data.temperature = Number(req.query.temperature) || 0;
+    data.voltage = Number(req.query.voltage) || 0;
+    data.ax1 = Number(req.query.ax1) || 0;
+    data.ay1 = Number(req.query.ay1) || 0;
+    data.az1 = Number(req.query.az1) || 0;
+    data.ax2 = Number(req.query.ax2) || 0;
+    data.ay2 = Number(req.query.ay2) || 0;
+    data.az2 = Number(req.query.az2) || 0;
+
+    sendUpdate();
+    res.json(data);
 });
 
-function sendTemperatureUpdate() {
+function sendUpdate() {
     clients.forEach(client => {
-        client.res.write(`data: ${JSON.stringify({ temperature })}\n\n`);
-    });	
+        client.res.write(`data: ${JSON.stringify(data)}\n\n`);
+    });
 }
 
 setInterval(() => {
@@ -54,7 +71,7 @@ setInterval(() => {
     });
 }, 25000);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
